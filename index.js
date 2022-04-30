b1cf843d7751c2bcc8fc6f165b187c124fb02f34
CMX\vgevorgyan <vgevorgyan@cinimex.ru>
2022-04-30 15:06:49 +0300

    modified index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // button => changing theme of the app
+const button = document.querySelector(".btn");
+
+function clicked() {
+    // get app element
+
+    // add to app element class .dark
+
+}
+
+button.addEventListener('click', clicked);
