9893d5f18335ec614870637c24a03f1142897788
CMX\vgevorgyan <vgevorgyan@cinimex.ru>
2022-04-30 15:27:35 +0300

    added switch logic in index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // button => changing theme of the app
 const button = document.querySelector(".btn");
-
+const app = document.querySelector(".app");
 function clicked() {
     // get app element
-
+    document.body.classList.toggle('dark');
     // add to app element class .dark
 
 }
