144ec29b2d172a788c9c14690a3f6d03a0703b5e
CMX\vgevorgyan <vgevorgyan@cinimex.ru>
2022-05-01 14:28:30 +0300

    modified theme switch button

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 // button => changing theme of the app
 const button = document.querySelector(".btn");
 const app = document.querySelector(".app");
+
 function clicked() {
     // get app element
-    document.body.classList.toggle('dark');
+    app.classList.toggle('dark');
     // add to app element class .dark
-
+    button.classList.toggle('dark-btn');
 }
 
 button.addEventListener('click', clicked);
